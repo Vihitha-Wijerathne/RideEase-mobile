934ca12dcd1a683e58deeaa2c4fee423e8b093ae
Poorna Theekshana <poornatheekshana@outlook.com>
2023-10-18 10:34:28 +0530

    Revert "Add loan Conflict Resolve"

--- app/src/main/java/com/example/rideease/Modals/RechargeModal.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rideease.Modals
-
-data class RechargeModal(
-    var rechargeId : String? = null,
-    var userId: String? = null,
-    var ramount : String? = null,
-//    var ldate : String? = null
-)
-
